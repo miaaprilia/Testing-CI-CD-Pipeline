fe00c78ee3bf2d3a04b7c264619e91c26120118b
Mia Aprilia Satya <miaa20010si@student.nurulfikri.ac.id>
2023-11-11 22:32:58 +0700

    initial commit

---
+++ src/test/java/starter/utils/JsonSchema.java
@@ -0,0 +1,6 @@
+package starter.utils;
+
+public class JsonSchema {
+    public static final String Login_Response_Schema = "schema/Login_Schema.json";
+    public static final String Login_Invalid_Email_Response_Schema = "schema/Login_Invalid_Email_Schema.json";
+    public static final String Login_Invalid_Password_Response_Schema = "schema/Login_Invalid_Password_Schema.json";}
